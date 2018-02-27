2a7ea2e0772ed7556874e644bec1b9446dcf91e6
Joshua Weinstein <jweinstein@splunk.com>
2018-02-26 19:18:37 -0800

    work for functional machine

--- fnmach.c
+++ fnmach.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
 /*
    Implementation of function machine,
    Works like a function but is a tangible object, like a VM.
@@ -25,6 +26,13 @@ typedef enum
         FnInst_AddI, // Adds an integer
         FnInst_SubI // Subtracts an integer
 } FnInst;
+
+// selection for running functional machine
+typedef enum
+{
+        FnMode_ret,
+        FnMode_recursive
+} FnMode;
 
 // Function Machine Structure
 // For this impl, only integers are used as types.
@@ -73,13 +81,49 @@ FnMach* FnMach_create(unsigned char* inst, size_t inst_n, int* args)
 {
         FnMach* newmach = FnMach_ALLOC;
         FnMach_load_params(newmach, args);
-        FnMach_load_ins(inst, inst_n);
+        FnMach_load_ins(newmach, inst, inst_n);
         return newmach;
+}
+
+// Variadic function that packs arguments into instructions for a function machine.
+// Returns 0 if error, 1 if success
+int FnMach_pack(FnMach* mach, const char* fmt, ...)
+{
+        unsigned char* writer = mach->ins;
+        va_list fnargs;
+        va_start(fnargs, fmt);
+        while(*fmt)
+        {
+                switch(*fmt)
+                {
+                case 'b':
+                        *writer++ = va_arg(fnargs, int);
+                        fmt++;
+                        break;
+                case 'i':
+                        *(int*)writer = va_arg(fnargs, int);
+                        writer += sizeof(int);
+                        fmt++;
+                        break;
+                default:
+                        fprintf(stderr, "Format Err:, unknown specifier %c\n", *fmt);
+                        va_end(fnargs);
+                        return 0;
+                }
+        }
+        va_end(fnargs);
+        return 1;
+}
+
+int FnMach_run(FnMach* mach, FnMode mode)
+{
+        return 1;
 }
 
 
 int main(int argc, char const *argv[]) {
         FnMach* machtest = FnMach_ALLOC;
+        FnMach_pack(machtest, "bi", 5, 500);
         FnMach_debug(machtest);
         free(machtest);
         return 0;
